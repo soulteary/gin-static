149dadfab0e929be1b0dc85bb5e4f698e722f599
Sergey Solodyagin <solodyagin@gmail.com>
2024-02-14 12:10:06 +0300

    feat: ServeCached

--- serve.go
+++ serve.go
@@ -1,6 +1,7 @@
 package static
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -17,12 +18,20 @@ func ServeRoot(urlPrefix, root string) gin.HandlerFunc {
 
 // Serve returns a middleware handler that serves static files in the given directory.
 func Serve(urlPrefix string, fs ServeFileSystem) gin.HandlerFunc {
+	return ServeCached(urlPrefix, fs, 0)
+}
+
+// ServeCached returns a middleware handler that similar as Serve but with the Cache-Control Header set as passed in the cacheAge parameter
+func ServeCached(urlPrefix string, fs ServeFileSystem, cacheAge uint) gin.HandlerFunc {
 	fileserver := http.FileServer(fs)
 	if urlPrefix != "" {
 		fileserver = http.StripPrefix(urlPrefix, fileserver)
 	}
 	return func(c *gin.Context) {
 		if fs.Exists(urlPrefix, c.Request.URL.Path) {
+			if cacheAge != 0 {
+				c.Writer.Header().Add("Cache-Control", fmt.Sprintf("max-age=%d", cacheAge))
+			}
 			fileserver.ServeHTTP(c.Writer, c.Request)
 			c.Abort()
 		}
